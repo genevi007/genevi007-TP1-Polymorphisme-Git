a70ff9b0aa4129b1a1dcc7e099394d10191c89fe
La Force <laforce@email.com>
2020-08-01 20:40:01 -0400

    Mise à jour de la classe Employee par Ghis

--- employee.cpp
+++ employee.cpp
@@ -1,27 +1,22 @@
 #include "employee.h"
 
-Employee::Employee(std::string le_nom, int le_matricule)
-{
-    this->nom = le_nom;
-    this->matricule = le_matricule;
+Employee::Employee(std::string le_nom, int le_matricule) {
+  this->nom = le_nom;
+  this->matricule = le_matricule;
+}
+    
+/*std::string Employee::getNom() {
+  return this->nom;
 }
 
-Employee::~Employee(){
-    }
-    
-std::string Employee::getNom() {
-return this->nom;
-}
-std::string Employee::setNom() {
-return this->nom;
-}
 int Employee::getMatricule() {
-return this->matricule;
+  return this->matricule;
 }
+
+std::string Employee::setNom() {
+  return this->nom;
+}
+
 int Employee::setMatricule() {
-    return this->matricule;
-}
-void Employee::afficher() {
-std::cout << "ERREUR ! Il est impossible d'afficher une employee.";
-std::cout << std::endl;
-}
+  return this->matricule;
+}*/
--- employee.h
+++ employee.h
@@ -1,28 +1,27 @@
-#ifndef EMPLOYEE_H
-#define EMPLOYEE_H
+#ifndef __EMPLOYEE_H__
+#define __EMPLOYEE_H__
 
 #include <iostream>
 #include <string>
 
-class Employee
-{
-protected:
-std::string nom;
-int matricule;
+class Employee{
+  protected:
+  std::string nom;
+  int matricule;
+  double paie_brute, impots_CA, impots_QC, paie_nette;
+  const double TAUX_IMPOTS_CA = 0.15;   //Taux CA de 15%
+  const double TAUX_IMPOTS_QC = 0.15;   //Taux QCde 15%
+  public:
+  Employee(std::string le_nom, int le_matricule);
+  virtual ~Employee(){std::cout << " - Employee détruite!" << std::endl;};
 
-public:
-Employee(std::string le_nom, int le_matricule);
-
-//Employee(int le_matricule);
-
-std::string getNom();
-std::string setNom();
-int getMatricule();
-int setMatricule();
-
-virtual void afficher()= 0;
-
-virtual ~Employee();
+  //std::string getNom();
+  //int getMatricule();
+  //std::string setNom();
+  //int setMatricule();
+  virtual double getPaie()=0;
+  virtual double getImpotsCA()=0;
+  virtual double getImpotsQC()=0;
 };
 
-#endif // EMPLOYEE_H
+#endif
