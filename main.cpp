19193a4a136aecec023a153c7c4d15f56b56b11c
La Force <laforce@email.com>
2020-08-10 10:40:44 -0400

    Petit Update du MAIN par Ghis

--- main.cpp
+++ main.cpp
@@ -6,16 +6,18 @@
 #include "ponctuelle.h"
 #include "syndiquee.h"
 
+
 int main() {
-  int choix = 0, matricule = 0;
-  std::string nom = "";
+  int choix=0, matricule=0, matricule_a_supprimer;
+  std::string nom="";
   Employee* employee[250];
   int nb_employee=0;
   double totaux_paies=0, totaux_impotsCA=0, totaux_impotsQC=0;
 
   std::cout << "GESTION DE PAIE - ABC INC." << std::endl;
   std::cout << "**************************";
-  do {// Menu employee
+   
+  do {      // Menu employee
     std::cout << std::endl << "Faites votre choix : " << std::endl;
     std::cout << " 1 - Ajouter unE employéE syndiquéE" << std::endl;
     std::cout << " 2 - Ajouter unE employéE contractuelLE" << std::endl;
@@ -25,24 +27,24 @@ int main() {
     std::cout << std::endl << "Votre choix : ";
     std::cin >> choix;
 
-    if (choix==1 || choix==2 || choix==3)  {       // Informations sur les employéEs.
+    if (choix==1 || choix==2 || choix==3) {     // Informations sur les employéEs.
       std::cout << " Nom de l'employéE : ";
       std::cin >> nom;
       do{
         std::cout << " Numéro matricule de l'employéE : ";
         std::cin >> matricule;
         if(matricule<10000 || matricule>99999)
-        std::cout << "Erreur! Le numéro de matricule doit-être entre 10000 et 99999." << std::endl;
-      }while (matricule<10000 || matricule>99999); 
+        std::cout << "Erreur! Le matricule doit-être entre 10000 et 99999." << std::endl;
+      }while (matricule<10000 || matricule>99999);
     }else if(choix==4 && nb_employee==0)
       std::cout << std::endl << " *ERREUR! Au moins unE employéE doit exister" << std::endl;
-else if (choix==0) {
-      std::cout << std::endl << " Bye! Bye!" << std::endl;
-    }else if (choix <0 && choix>5)
-      std::cout << std::endl << " *ERREUR! Faites un choix entre 0 et 5." << std::endl;
-      
-    switch(choix){
-      case 1:{                // EmployéE SyndiquéE
+    else if (choix<0 || choix>4) {
+      std::cout << std::endl << " *ERREUR! Faites un choix entre 0 et 4." << std::endl;
+    }else if (choix==0)
+      std::cout << std::endl << "Bye! Bye!" << std::endl;
+
+    switch(choix) {
+      case 1:{      // EmployéE SyndiquéE
         double salaire=0;
         double nbHeures=0;
         std::cout << " Salaire horaire de l'employéE syndiquéE : ";
@@ -66,7 +68,7 @@ else if (choix==0) {
         break;
       }
 
-      case 3:{        // EmployéE PonctuelLE
+      case 3:{      // EmployéE PonctuelLE
         double paye=0;
         std::cout << " Montant payé à l'employéE ponctuelLE : ";
         std::cin >> paye;
@@ -75,7 +77,7 @@ else if (choix==0) {
         break;
       }
 
-      case 4:{        // Afficher les employéEs
+      case 4:{      // Afficher les employéEs
         std::cout << std::endl;
         for (int i=0 ; i<nb_employee ; i++) {
           totaux_paies = totaux_paies + employee[i]->getPaie();
@@ -88,13 +90,14 @@ else if (choix==0) {
           std::cout << " Montants totaux versés de " << totaux_paies << " $" << std::endl;
           std::cout << " Impots à transferer CA de " << totaux_impotsCA << " $" << std::endl;
           std::cout << " Impots à transferer QC de " << totaux_impotsQC << " $" << std::endl;
+          std::cout << std::endl;
         }
         break;
       }
     }
-  }while(choix !=0);
+  }while(choix!=0);
 
-  for (int i=0 ; i<nb_employee ; i++) {  // Supprimer employées créées dynamiquement
+  for (int i=0 ; i<nb_employee ; i++) {     // Supprimer employéEs créÉes dynamiquement
     delete employee[i];
   }
 }
